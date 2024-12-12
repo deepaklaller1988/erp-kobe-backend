585e11943d8a9d53e3309aaf2626a4a355ff4492
dinesh <dlaller1988@gmail.com>
2024-12-12 10:07:17 +0530

    api

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import accessControl from '../middlewares/access-control';
-import { getAllOrdersOfaProduct, getAllOrdersOfaSeller } from '../controllers/order.controller';
+import { createOrder, getAllOrdersOfaProduct, getAllOrdersOfaSeller } from '../controllers/order.controller';
 
 const router = express();
 
 router.use(accessControl);
 router.route('/by-seller').get(getAllOrdersOfaSeller);
-router.route('/by-product').get(getAllOrdersOfaProduct);
+// router.route('/by-product').get(getAllOrdersOfaProduct);
+router.route('/by-order').post(createOrder);
+
 
 export default router;
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import accessControl from '../middlewares/access-control';
-import { getAllProductsOfaSeller } from '../controllers/product.controller';
+import { createProduct, getAllProductsOfaSeller } from '../controllers/product.controller';
 
 const router = express();
 
 router.use(accessControl);
-router.route('/').get(getAllProductsOfaSeller);
+router.route('/').get(getAllProductsOfaSeller).post(createProduct);
 
 export default router;
