66f6de72df163c8d45d67abbbf406894c6a6784c
dinesh <dlaller1988@gmail.com>
2024-12-17 10:51:51 +0530

    authroutes 17 dec 2024

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,4 +1,4 @@
-import accessControl from "../middlewares/access-control";
+// import accessControl from "../middlewares/access-control";
 import { activateAccount, forgotPassword, login, logout, register, resetPassword } from "../controllers/auth.controller";
 import express from "express";
 
@@ -6,7 +6,7 @@ const router = express();
 
 router.route('/login').post(login);
 router.route('/register').post(register);
-router.route('/logout').get(accessControl, logout);
+router.route('/logout').get(logout);
 router.route('/account-activation').get(activateAccount);
 router.route('/forgot-password').post(forgotPassword);
 router.route('/password-reset').post(resetPassword);
